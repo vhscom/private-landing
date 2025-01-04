02601e4708b8229658bcdf91f1b731815bcd797c
vhs <vhsdev@tutanota.com>
2025-01-04 11:29:54 -0500

    serve static assets

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,40 @@
+import type { Fetcher } from "@cloudflare/workers-types";
 import { Hono } from "hono";
+import { createMiddleware } from "hono/factory";
 import "./db.ts";
 
 const app = new Hono<{ Bindings: Env }>();
 
-app.get("/", (c) => {
-	return c.text("Hello via index route handler!");
-	// return c.json({
+type ServeStaticOptions = {
+	cache: string;
+};
+
+function serveStatic(opts: ServeStaticOptions) {
+	return createMiddleware(async (ctx, next) => {
+		const binding = ctx.env.ASSETS as Fetcher;
+		const response = await binding.fetch(
+			ctx.req.url,
+			/**
+			 * Clone raw request and coerce to Cloudflare RequestInit type.
+			 * @example
+			 * import type {
+			 *    RequestInit as CfRequestInit,
+			 *    CfProperties,
+			 * } from "@cloudflare/workers-types";
+			 * ctx.req.raw.clone() as unknown as CfRequestInit<CfProperties>,
+			 */
+		);
+
+		if (!response.ok) return await next();
+		return response as unknown as globalThis.Response;
+	});
+}
+
+app.use("*", serveStatic({ cache: "key" }));
+
+app.get("/", (ctx) => {
+	return ctx.text("Hello via index route handler!");
+	// return ctx.json({
 	//   data: 'Hello from the JSON response!',
 	// });
 });
